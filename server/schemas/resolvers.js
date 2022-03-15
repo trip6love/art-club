2f8b92a5b7e18ee44ab84de279e591e2d04d1260
rylee thomas <ryleetreat@gmail.com>
2022-03-15 01:17:34 -0400

    updated comment and post routes

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,7 +1,27 @@
+const { User, Post } = require('../models');
+
 const resolvers = {
     Query: {
-      helloWorld: () => {
-        return 'Hello world!';
+      //get all posts
+      posts: async (parent, { username }) => {
+        const params = username ? { username } : {};
+        return Post.find(params).sort({ createdAt: -1});
+      },
+      //get post by id
+      post: async(parent, {_id}) => {
+        return Post.findOne({_id});
+      },
+      //get all users
+      users: async() => {
+        return User.find()
+            .select('-__v -password')
+            .populate('comments');
+      },
+      //get user by username
+      user: async(parent, {username}) => {
+        return User.findOne({ username })
+            .select('-__v -password')
+            .populate('comments');
       }
     }
   };
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -3,8 +3,37 @@ const { gql } = require('apollo-server-express');
 
 // create our typeDefs
 const typeDefs = gql`
+
+  type User {
+    _id: ID
+    username: String
+    email: String
+    commentCount: Int
+    comments: [Comment]
+  }
+
+  type Post {
+    _id: ID
+    postTitle: String
+    postText: String
+    CreatedAt: String
+    username: String
+    commentCount: Int
+    comments: [Comment]
+  }
+
+  type Comment {
+    _id: ID
+    commentBody: String
+    createdAt: String
+    username: String
+  }
+
   type Query {
-    helloWorld: String
+    users: [User]
+    user(username: String!): User
+    posts(username: String): [Post]
+    post(_id: ID!): Post
   }
 `;
 
