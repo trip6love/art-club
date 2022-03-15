92d8def917c7aee0e4e7f5a456fc56fefa81ef96
rylee thomas <ryleetreat@gmail.com>
2022-03-15 00:00:39 -0400

    added server folder with models

---
+++ server/models/Post.js
@@ -0,0 +1,47 @@
+const { Schema, model } = require('mongoose');
+const commentSchema = require('./Comment');
+const dateFormat = require('../utils/dateFormat');
+
+const postSchema = new Schema(
+  {
+    postTitle: {
+        type: String,
+        required: 'You need a title',
+        minlength: 1,
+        maxlength: 20
+    },
+    postText: {
+      type: String,
+      required: 'You need a Comment',
+      minlength: 1,
+      maxlength: 280
+    },
+    postImage: {
+        type: URL,
+        required: false
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: timestamp => dateFormat(timestamp)
+    },
+    username: {
+      type: String,
+      required: true
+    },
+    comments: [commentSchema]
+  },
+  {
+    toJSON: {
+      getters: true
+    }
+  }
+);
+
+postSchema.virtual('commentCount').get(function() {
+  return this.comments.length;
+});
+
+const Post = model('Post', postSchema);
+
+module.exports = Post;
