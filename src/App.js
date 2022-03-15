681c62958dbe89c7978eec2b016500deb4ab2cf5
trip6love <lanebyers13@gmail.com>
2022-03-14 20:39:52 -0400

    App js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,40 @@ import React, { useState } from 'react';
 
 
 function App() {
-  
+  const [currentPage, setCurrentPage] = useState('Login');
+
+  const renderPage = () => {
+        if (currentPage === 'Login') {
+          return <Login />
+        }
+        if (currentPage === 'Artboard') {
+            return <Artboard />
+        }
+        if (currentPage === 'SignUp') {
+            return <SignUp />
+        }
+        if (currentPage === 'Post') {
+            return <Post />
+        }
+        if (currentPage === 'Logout') {
+            return <Logout />
+        }
+        if (currentPage === 'Account') {
+            return <Account />
+        }
+    };
+
+    const handlePageChange = (page) => setCurrentPage (page);
+
+    return (
+        <div>
+            <Header />
+            <Navbar currentPage={currentPage} handlePageChange={handlePageChange} />
+            {renderPage()}
+            <Footer />
+            
+        </div>
+    )
 }
 
 export default App;
