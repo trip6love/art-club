eb1ec2672539addacee698c5b83086d052297b31
parkerjustice <you@example.com>
2022-03-19 18:11:08 -0400

    added ArtClub logo in rainbow

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -3,7 +3,15 @@ import Navbar from './Navbar';
 function Header () {
     return(
         <header>
-            <h1 className="header rainbow-text">Art Club</h1>
+            <h1 className="header rainbow-text">
+            <span className='a'>A</span>
+            <span className='r'>r</span>
+            <span className='t'>t</span>
+            <span className='c'>C</span>
+            <span className='l'>l</span>
+            <span className='u'>u</span>
+            <span className='b'>b</span>
+            </h1>
             <Navbar></Navbar>
         </header>
         
