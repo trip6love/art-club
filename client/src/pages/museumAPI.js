7e3f14dfcd73730272d4429e057cf1acf1cad98e
rylee thomas <ryleetreat@gmail.com>
2022-03-21 20:11:28 -0400

    updated api error

--- client/src/pages/museumAPI.js
+++ client/src/pages/museumAPI.js
@@ -1,15 +1,39 @@
 
-import { searchArtMuseum } from '../utils/apiRoutes';
-import React from 'react';
+//import { searchArtMuseum } from '../utils/apiRoutes';
+import React, { useState, useEffect } from 'react';
 
-const museumAPI = () => {
-    searchArtMuseum();
+const MuseumAPI = () => {
+    //create state for holding return google api data
+    const [ loading, setLoading] = useState(true);
+    const [ items, setItems] = useState();
+
+    useEffect( async () => {
+        try {
+            //const response = await searchArtMuseum();
+    
+            const response = await fetch("https://api.harvardartmuseums.org/image?apikey=d5fa0071-2bef-4273-9a6c-c6fad3b41af9");
+    
+            if(!response){
+                throw new Error('Something is wrong');
+            }
+    
+            setItems( await response.json());
+            setLoading(false);
+        } catch (err) {
+            console.error(err);
+        }
+    },[])
+
+
     return (
         <>
+            {!loading && (
+                console.log(items)
+            )}
             <p> API info here</p>
         </>
     )
 
 };
 
-export default museumAPI;
+export default MuseumAPI;
--- client/src/utils/apiRoutes.js
+++ client/src/utils/apiRoutes.js
@@ -1,11 +1,12 @@
 // Find all of the objects that are paintings and have the word "rabbit" in the title
 var apiEndpointBaseURL = "https://api.harvardartmuseums.org/object";
 
-export const searchArtMuseum = () => {
+export const searchArtMuseum =  async () => {
 
     var artCall = "https://api.harvardartmuseums.org/image?apikey=d5fa0071-2bef-4273-9a6c-c6fad3b41af9";
     console.log(artCall);
-    fetch(artCall)
+    return await fetch(artCall);
+    /*
     .then(function (response) {
         if(response.ok) {
             response.json().then(function (data) {
@@ -13,4 +14,5 @@ export const searchArtMuseum = () => {
             })
         }
     })
+    */
 };
