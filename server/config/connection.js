c65337d0f39f52ad6bdb2529efb1fd40ed8bd100
trip6love <lanebyers13@gmail.com>
2022-03-22 22:45:24 -0400

    form styles

--- server/config/connection.js
+++ server/config/connection.js
@@ -13,6 +13,5 @@ mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/art-club', {
   //useCreateIndex: true,
   //useFindAndModify: false
 //});
-//'mongodb://localhost/art-club'
 
 module.exports = mongoose.connection;
