1d0b262ca1e4f5225ef3ecb9d6213141ac9465c9
parkerjustice <you@example.com>
2022-03-23 13:16:05 -0400

    added new commentForm, added inspiration page

--- client/src/components/CommentForm.js
+++ client/src/components/CommentForm.js
@@ -3,71 +3,82 @@ import React, { useState } from 'react';
 
 import { useMutation } from '@apollo/client'; 
 import { ADD_COMMENT } from '../../src/utils/mutations'; 
+import { QUERY_POST } from '../../src/utils/queries';
 
-const CommentForm = ({ commentId }) => {
+const CommentForm = ({postId}) => {
     const [commentBody, setBody] = useState(''); 
     const [characterCount, setCharacterCount] = useState(0); 
-    const [addComment, { error }] = useMutation(ADD_COMMENT);
 
-    const handleChange = (event) => {
-        if (event.target.value.length <= 500) {
-            setBody(event.target.value); 
-            setCharacterCount(event.target.value.length); 
+    const [addComment, { error }] = useMutation(ADD_COMMENT, {
+        //refresh the new list
+        update(cache, {data: { addComment } }) {
+            try {
+                //could potentially not exist yet
+                const { comments } = cache.readQuery({ query: QUERY_POST }); 
+                cache.writeQuery({
+                    query: QUERY_POST, 
+                    data: { posts: [addComment, ...comments] },
+                });
+            } catch (e) {
+                console.log(e);
+            } 
+        },
+    });
+
+    //handle text input change
+    const handleCommChange = event => {
+        if (event.target.value.length <= 280) {
+          setBody(event.target.value);
+          setCharacterCount(event.target.value.length);
         }
-    }; 
+    };
 
+    //adds comments to post comments[]
     const handleFormSubmit = async (event) => {
-        event.preventDefault(); 
-
         try {
+            //add comment to database
             await addComment({
-                variables: { commentBody, commentId }, 
+                variables: { postId, commentBody },
             });
 
-            setBody(''); 
+            //clear form value
+            setBody('');
             setCharacterCount(0);
+
         } catch (e) {
-            console.error(e); 
+            console.error(e);
         }
-    }; 
+    };
 
     return (
         <div>
-
             <div class="create-post">
                 <div class="create-post-header">
-                <h2 class="create-post-title" id="post-header-title"> Create A Comment</h2>
+                    <h2 class="create-post-title" id="post-header-title"> Add a Comment</h2>
                 </div>
+                <form id="create-form" class="create-post-form" name="form">
+
+                    <div class="create-post-content">
+                        <label className={`${characterCount === 280 || error ? 'text-error' : ''}`}>
+                            Character Count: {characterCount}/280
+                            {error && <span className=''>Something Went Wrong</span>}
+                        </label>
+                        <textarea name="post" 
+                            id="post-text" 
+                            class="create-post-textarea scroller"
+                            placeholder="Add your comment here.."
+                            value={commentBody}
+                            onChange={handleCommChange}
+                        ></textarea>
+                    </div>
+
+                    <div class="create-post-actions post-actions">
+                        <div class="post-actions-create">
+                            <button class="btn post-actions-publish" onClick={ () => handleFormSubmit()}>Submit</button>
+                        </div>
+                    </div>
+                </form>
             </div>
-
-            <form id="create-form" class="create-post-form" name="form">
-
-                <div class="create-post-content">
-                    <textarea name="post" 
-                    id="post-title" 
-                    class="create-post-textarea scroller"
-                    placeholder="Post Title">
-                    </textarea>
-                </div>
-
-                <div class="create-post-content">
-                <textarea name="post" 
-                id="post-text" 
-                class="create-post-textarea scroller"
-                placeholder="Add your comment here..">
-                </textarea>
-                </div>
-
-                <div class="create-post-actions post-actions">
-
-                <div class="post-actions-create">
-                    <button class="btn post-actions-publish">Submit</button>
-                </div>
-
-                </div>
-
-            </form>
-
             {error && <div>Something went wrong...</div>}
         </div>
     );
