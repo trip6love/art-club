5811ece9f89a0f01354bdae9fd8f58e67b635a30
rylee thomas <ryleetreat@gmail.com>
2022-03-22 23:49:11 -0400

    this broke somehow

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,11 +1,14 @@
+
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://parker47:butter47@cluster0.m3dkw.mongodb.net/artclub?retryWrites=true&w=majority', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/art-club', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
   useFindAndModify: false
 });
+
+module.exports = mongoose.connection;
 
 //mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://trip6love:Homer123@cluster0.refbv.mongodb.net/artclub?retryWrites=true&w=majority', {
   //useNewUrlParser: true,
